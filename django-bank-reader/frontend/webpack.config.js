1ef7d0afc3c5de400a1cf4d15ff5f57fa68d251d
Silvano Cerza <silvanocerza@gmail.com>
2018-01-04 18:46:40 +0100

    fixed webpack config indentation and disabled fail on warning for eslint

--- django-bank-reader/frontend/webpack.config.js
+++ django-bank-reader/frontend/webpack.config.js
@@ -6,61 +6,61 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const production = process.env.NODE_ENV === 'production';
 
 const extractSass = new ExtractTextPlugin({
-    filename: '../css/main.css',
+  filename: '../css/main.css',
 });
 
 let plugins = [
-    extractSass,
-    new webpack.LoaderOptionsPlugin({
-        debug: !production
-    })
-]
+  extractSass,
+  new webpack.LoaderOptionsPlugin({
+    debug: !production,
+  }),
+];
 
 if (production) {
-    plugins = plugins.concat([
-        new UglifyJSPlugin()
-    ]);
+  plugins = plugins.concat([
+    new UglifyJSPlugin(),
+  ]);
 }
 
 module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'static/js')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: extractSass.extract({
-                    use: [{
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true,
-                        }
-                    }, {
-                        loader: 'sass-loader'
-                    }],
-                    fallback: 'style-loader'
-                })
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'static/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader',
+            options: {
+              minimize: true,
             },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-                options: {
-                    failOnWarning: true,
-                    failOnError: true,
-                    configFile: './.eslintrc.json'
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: plugins
-}
+          }, {
+            loader: 'sass-loader',
+          }],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          failOnWarning: false,
+          failOnError: true,
+          configFile: './.eslintrc.json',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins,
+};
