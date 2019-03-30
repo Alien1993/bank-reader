ab5df5b832b4c91e1557f1548777a25c24868178
Silvano Cerza <silvanocerza@gmail.com>
2019-03-30 17:22:28 +0100

    Started frontend rewrite

--- django-bank-reader/frontend/src/js/index.js
+++
@@ -1,9 +0,0 @@
-require('babel-polyfill');
-require('../../node_modules/pikaday/scss/pikaday.scss');
-require('../css/main.scss');
-require('./client.js');
-require('./datepicker.js');
-require('./form-controller.js');
-require('./processors.js');
-require('./bubble-chart-controller.js');
-require('./bar-chart-controller.js');
