1528e56a37f8ec4f472bc852683d0f7b0f4bb078
Silvano Cerza <silvanocerza@gmail.com>
2019-04-03 22:23:31 +0200

    Implemented Form component

--- django-bank-reader/frontend/src/App.vue
+++ django-bank-reader/frontend/src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
+    <Form/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import Form from './components/Form.vue';
 
-@Component
+@Component({
+  components: {
+    Form,
+  },
+})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "bulma";
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 3rem;
+  margin-left: 3rem;
+  margin-right: 3rem;
 }
 </style>
---
+++ django-bank-reader/frontend/src/components/Form.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav class="panel">
+    <p v-if="title" class="panel-heading">
+      {{ title }}
+    </p>
+    <div class="panel-block">
+      <div class="field">
+        <label class="label">Date from</label>
+        <div class="control">
+          <datepicker
+            :input-class="inputClass"
+            :value="fromDate"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="panel-block">
+      <div class="field">
+        <label class="label">Date to</label>
+        <div class="control">
+          <datepicker
+            :input-class="inputClass"
+            :value="toDate"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="panel-block">
+      <button
+        class="button is-primary is-fullwidth"
+        @click="getMovements"
+      >
+        Submit
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Datepicker from 'vuejs-datepicker';
+import moment from 'moment';
+
+@Component({
+  components: {
+    datepicker: Datepicker,
+  },
+})
+export default class Form extends Vue {
+@Prop({ default: 'input' }) public readonly inputClass!: string;
+@Prop(String) private title!: string;
+@Prop({ default: moment().subtract(2, 'weeks').valueOf() }) private fromDate!: moment.Moment;
+@Prop({ default: moment().valueOf() }) private toDate!: moment.Moment;
+
+  private getMovements() {
+    // TODO: Here call API
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.panel {
+  position: fixed;
+  width: 20%;
+}
+</style>
